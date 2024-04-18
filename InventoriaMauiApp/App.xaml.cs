8ea820ac951a77872773b0f859712706ed7a4921
Rene Christensen <rene723@gmail.com>
2024-04-18 20:31:02 +0200

    Add project files.

---
+++ InventoriaMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoriaMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ InventoriaMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoriaMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
