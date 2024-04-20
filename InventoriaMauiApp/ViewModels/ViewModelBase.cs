1919b5ffa0ce85839825a3bc8dea7d5f7992db27
Rene Christensen <rene723@gmail.com>
2024-04-20 12:29:54 +0200

    Fixed warnings in viewmodelbase

--- InventoriaMauiApp/ViewModels/ViewModelBase.cs
+++ InventoriaMauiApp/ViewModels/ViewModelBase.cs
@@ -6,14 +6,14 @@
 
     public class ViewModelBase : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        protected bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
         {
             if (EqualityComparer<T>.Default.Equals(field, value)) return false;
             field = value;
